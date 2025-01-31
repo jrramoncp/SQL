b17522c10a56dbf6b4c6a0d661defd124ab5dcf4
Jose Ramón Caravaca <joseramoncaravaca@gmail.com>
2025-01-31 06:17:09 +0100

    Corregidos ejercicios

--- clase14/clase14EjerciciosGroupBY2.sql
+++ clase14/clase14EjerciciosGroupBY2.sql
@@ -9,10 +9,10 @@ from world.city inner join world.country
 on city.CountryCode = country.Code
 group by CountryCode;
 -- 11. Encuentra los continentes cuya población total es superior a 100 millones
-select Continent, SUM(Population) as Población
+select Continent, SUM(Population) as Poblacion
 from world.country
 Group By Continent
-having Population > 1000000000;
+having Poblacion > 100000000;
 -- 12. Encuentra la cantidad de idiomas oficiales en cada país
 select * from world.countrylanguage;
 select countrylanguage.CountryCode, country.Name, count(countrylanguage.Language) as lenguas_oficiales
@@ -35,7 +35,7 @@ from world.countrylanguage inner join world.country
 on countrylanguage.CountryCode = country.Code
 group by countrylanguage.language;
 -- 15. Encuentra los continentes donde ningún país tiene una población superior a 200 millones
-select Continent, sum(Population) as Poblacion_total from world.country
+select Continent, max(Population) as Poblacion_total from world.country
 group by Continent
 having Poblacion_total < 200000000;
 -- 16. Encuentra los continentes donde la cantidad de países con una expectativa de vida superior a 80 años sea igual o mayor a 3
@@ -44,7 +44,7 @@ select continent, count(LifeExpectancy) as Esperanza_Vida
 from world.country
 where LifeExpectancy > 80
 group by continent
-having Esperanza_Vida < 3;
+having Esperanza_Vida >= 3;
 -- 17. Encuentra el promedio de la expectativa de vida en cada continente, excluyendo aquellos donde el promedio sea inferior a 70 años
 select * from world.country;
 Select Continent, AVG(LifeExpectancy) as Esperanza_vida
